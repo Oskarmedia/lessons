f421473fec74f4c0f102a329856d7c10ed07dbcc
ramanich <fixnestapp@gmail.com>
2026-01-10 20:06:21 -0800

    cycle for text

--- lesson1.dart
+++ lesson1.dart
@@ -1,5 +1,8 @@
 void main() {
   var text = 'Hello, Dart!';
-
   print(text);
+  print(text.length);
+   for (var i = 0; i < text.length; i++) {
+    print('Character at index $i: ${text[i]}');
+   }
 }
